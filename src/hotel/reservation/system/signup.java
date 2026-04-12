6ef776a47c5280191ec3cfa1edd8351823f33d93
Naige30 <ishigamisenku0603@gmail.com>
2026-04-12 22:09:52 +0800

    Date added

--- src/hotel/reservation/system/signup.java
+++ src/hotel/reservation/system/signup.java
@@ -14,9 +14,10 @@ import java.awt.*;
  */
 public class signup extends JFrame implements ActionListener {
     private JTextField userinp,emailinp,numberinp;
-    private JLabel userlbl,passlbl,emaillbl,numberlbl,titlelbl;
+    private JLabel userlbl,passlbl,emaillbl,numberlbl,titlelbl,bdaylbl;
     private JButton submitbtn;    
     private JPasswordField passinp; 
+    private JComboBox<String>monthBox,dayBox,yearBox;
     signup(){
         setSize(374,550);
         setLayout(null);
@@ -59,8 +60,35 @@ public class signup extends JFrame implements ActionListener {
         numberinp.setBounds(110,220,200,30);
         add(numberinp);
         
+        bdaylbl=new JLabel("Birthday:");
+        bdaylbl.setBounds(30,270,90,30);
+        add(bdaylbl);
+        
+        String[]months={"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
+        monthBox=new JComboBox<>(months);
+        monthBox.setBounds(110,270,70,30);
+        add(monthBox);
+        
+        String[]day=new String [31];
+        for(int i=1;i<=31;i++){
+            day[i-1]=String.valueOf(i);
+        }
+        dayBox=new JComboBox<>(day);
+        dayBox.setBounds(185,270,60,30);
+        add(dayBox);
+        
+        String[]years=new String[127];
+        int startYear=1900;
+        for(int i=0;i<years.length;i++){
+            years[i]=String.valueOf(startYear+i);
+        }
+        yearBox=new JComboBox<>(years);
+        yearBox.setBounds(250,270,70,30);
+        add(yearBox);
+        
+      
         submitbtn=new JButton("Submit");
-        submitbtn.setBounds(170,270,90,30);
+        submitbtn.setBounds(140,450,100,30);
         add(submitbtn);
        
                 
